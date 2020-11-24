4e274be65fe921def3d844f2214be9d7f0e2be59
Golovizin Danil <danil@rutorika.ru>
2020-11-25 01:09:55 +0300

    test responses

--- app/Helpers/BotMessagesListener.php
+++ app/Helpers/BotMessagesListener.php
@@ -4,6 +4,8 @@
 namespace App\Helpers;
 
 
+use App\Jobs\BotSendMessage;
+use App\Models\Message;
 use danog\MadelineProto\EventHandler;
 use danog\MadelineProto\RPCErrorException;
 
@@ -13,6 +15,28 @@ class BotMessagesListener extends EventHandler
      * @var int|string Username or ID of bot admin
      */
     const ADMIN = "ruportbot"; // Change this
+
+    private function _validate($text, $code) {
+        $rules = [
+            Message::CODES['ENTER_BDATE'] => [
+                'text' => 'regex:/\d\d\.\d\d.\d\d\d\d/'
+            ]
+        ];
+
+        return Validator::make(["text" => $text], $rules[$code])->validate();
+    }
+
+    private function _makeResponse($memberId, $messageText)
+    {
+        $lastMessage = Message::whereMemberId($memberId)->latest();
+        if(!$lastMessage) {
+            return BotSendMessage::dispatch($memberId, Message::CODES['NO_REQUEST']);
+        }
+
+        if(!$this->_validate($messageText, $lastMessage->message_code)) {
+            return BotSendMessage::dispatch($memberId, $lastMessage->message_code);
+        }
+    }
 
     /**
      * Get peer(s) where to report errors
@@ -61,16 +85,13 @@ class BotMessagesListener extends EventHandler
         $res = \json_encode($update, JSON_PRETTY_PRINT);
 
         try {
+            $this->_makeResponse($update['message']['user_id'], $update['message']['message']);
+
             yield $this->messages->sendMessage(['peer'            => $update,
                                                 'message'         => "<code>$res</code>",
                                                 'reply_to_msg_id' => isset($update['message']['id']) ? $update['message']['id'] : null,
                                                 'parse_mode'      => 'HTML'
             ]);
-            $messageText = $update['message']['message'];
-            $memberId = $update['message']['user_id'];
-
-            $response = new BotResponseManager();
-            $response->makeResponse($memberId, $messageText);
         } catch (RPCErrorException $e) {
             $this->report("Surfaced: $e");
         } catch (Exception $e) {
